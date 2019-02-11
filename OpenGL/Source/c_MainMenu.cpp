3a89763215b79c21e4c976f419894f78eb28e770
Edward095 <42329335+Edward095@users.noreply.github.com>
2019-02-11 17:53:59 +0800

    d

--- OpenGL/Source/c_MainMenu.cpp
+++ OpenGL/Source/c_MainMenu.cpp
@@ -1,6 +1,14 @@
 #include "c_MainMenu.h"
+#include "GL\glew.h"
+
+#include "shader.hpp"
+#include "Mtx44.h"
+
 #include "Application.h"
 #include "MeshBuilder.h"
+
+#include "Vertex.h"
+#include "Utility.h"
 #include "LoadTGA.h"
 
 
@@ -12,28 +20,77 @@ c_MainMenu::c_MainMenu()
 c_MainMenu::~c_MainMenu()
 {
 }
-
 void c_MainMenu::Init()
 {
-	APosX = 0;
-	APosY = 0;
-	APosZ = 0;
+	// Set background color to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	//Enable depth buffer and depth testing
+	glEnable(GL_DEPTH_TEST);
+	//Enable back face culling
+	glEnable(GL_CULL_FACE);
+	//Default to fill mode
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	// Enable blending
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	meshList[NEWGAME] = MeshBuilder::GenerateQuad("NewGame", RED, 5);
-	meshList[NEWGAME]->textureID = LoadTGA("Image//NewGame.tga");
+	// Generate a default VAO for now
+	glGenVertexArrays(1, &m_vertexArrayID);
+	glBindVertexArray(m_vertexArrayID);
+
+	//Load vertex and fragment shaders
+	//m_programID = LoadShaders("Shader//Texture.vertexshader", "Shader//Texture.fragmentshader"); //before text
+	m_programID = LoadShaders("Shader//Texture.vertexshader", "Shader//Text.fragmentshader");
+
+	// Use our shader
+	glUseProgram(m_programID);
+
+	// Get a handle for our "MVP" uniform
+	m_parameters[U_MVP] = glGetUniformLocation(m_programID, "MVP");
+	m_parameters[U_MODELVIEW] = glGetUniformLocation(m_programID, "MV");
+	m_parameters[U_MODELVIEW_INVERSE_TRANSPOSE] = glGetUniformLocation(m_programID, "MV_inverse_transpose");
+
+	m_parameters[U_MATERIAL_AMBIENT] = glGetUniformLocation(m_programID,
+		"material.kAmbient");
+	m_parameters[U_MATERIAL_DIFFUSE] = glGetUniformLocation(m_programID,
+		"material.kDiffuse");
+	m_parameters[U_MATERIAL_SPECULAR] = glGetUniformLocation(m_programID,
+		"material.kSpecular");
+	m_parameters[U_MATERIAL_SHININESS] = glGetUniformLocation(m_programID,
+		"material.kShininess");
+
+	//Text
+	m_parameters[U_TEXT_ENABLED] = glGetUniformLocation(m_programID,
+		"textEnabled");
+	m_parameters[U_TEXT_COLOR] = glGetUniformLocation(m_programID,
+		"textColor");
+	//Initialize camera settings
+	camera.Init(Vector3(0, 0, 15));
+
+	//Initialize all meshes to NULL
+	for (int i = 0; i < NUM_GEOMETRY; ++i)
+	{
+		meshList[i] = NULL;
+	}
+
+	//Set projection to Perspective and load projection matrix
+	Mtx44 projection;
+	projection.SetToPerspective(45.f, 4.f / 3.f, 0.1f, 10000.f);
+	projectionStack.LoadMatrix(projection);
 }
-
 void c_MainMenu::Update(double dt)
 {
-	if (Application::IsKeyPressed(VK_UP))
+	if (Application::IsKeyPressed(VK_SPACE))
 	{
 
 	}
 }
-
 void c_MainMenu::Render()
 {
-	modelStack.PushMatrix();
-	render.RenderMesh(meshList[NEWGAME], false);
-	modelStack.PopMatrix();
+
+
+}
+void c_MainMenu::Exit()
+{
+
 }
