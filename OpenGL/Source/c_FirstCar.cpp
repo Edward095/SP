5cff22410e51bc6e5b1400a1b932ea40007739b0
LAPTOP-9IQQMDO2\irfan <shaikhmdirfan20@gmail.com>
2019-02-13 16:34:33 +0800

    CarMovement

--- OpenGL/Source/c_FirstCar.cpp
+++ OpenGL/Source/c_FirstCar.cpp
@@ -1,12 +1,12 @@
 #include "c_FirstCar.h"
 #include "Application.h"
+#include "MyMath.h"
 
 c_FirstCar::c_FirstCar()
 {
-	driving = false;
+	Driving = false;
 	VelocityZ = 0;
-	acceleration = 0;
-	VelocityX = 0;
+	Acceleration = 0;
 }
 
 c_FirstCar::~c_FirstCar()
@@ -16,72 +16,142 @@ c_FirstCar::~c_FirstCar()
 
 void c_FirstCar::Movement(double dt)
 {
-	if (Application::IsKeyPressed('W'))
-	{
-		acceleration += 0.1;
-		VelocityZ += acceleration * dt;
-		driving = true;
-		backwards = false;
-
-		if (acceleration > 30)
+	
+		if (Application::IsKeyPressed('W'))
 		{
-			acceleration = 30;
+			Acceleration += 0.1;
+			//steeringAngle = 0;
+			VelocityZ += Acceleration * dt;
+			NewPosX += (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			NewPosY = NewPosY;
+			NewPosZ += (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			Driving = true;
+			Backwards = false;
+
+			if (Acceleration > 1)
+			{
+				Acceleration = 1;
+			}
+			if (VelocityZ > 1)
+			{
+				VelocityZ = 1;
+			}
+
 		}
-	}
-	if (driving)
+
+
+
+	if (Driving)
 	{
 		if (!Application::IsKeyPressed('W'))
 		{
-			acceleration -= 0.5;
-			VelocityZ += acceleration * dt;
-
-			if (acceleration <0)
+			Acceleration -= 0.1;
+			VelocityZ += Acceleration * dt;
+			NewPosX += (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			NewPosY = NewPosY;
+			NewPosZ += (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			if (Acceleration < 0)
 			{
-				acceleration = 0;
-				driving = false;
+				Acceleration = 0;
+				VelocityZ -= 0.05;
+				//driving = false;
 			}
-		}
+			if (VelocityZ < 0)
+			{
+				VelocityZ = 0;
+				Driving = false;
+				Backwards = true;
+			}
 
+		}
 	}
 
 
+	if (Application::IsKeyPressed('D'))
+	{
+		if (Driving == true)
+		{
+			SteeringAngle -= 3;
+		}
+		if (Backwards == true)
+		{
+			SteeringAngle += 3;
+		}
+		
+	}
 
+
+	if (Application::IsKeyPressed('A'))
+	{
+		if (Driving == true)
+		{
+			SteeringAngle += 3;
+		}
+		if (Backwards == true)
+		{
+			SteeringAngle -= 3;
+		}
+	}
 
 
 	if (Application::IsKeyPressed('S'))
 	{
-		acceleration -= 0.1;
-		VelocityZ += acceleration * dt;
-		backwards = true;
-		driving = false;
+		Acceleration -= 0.1;
+		VelocityZ += Acceleration * dt;
+		NewPosX += (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+		NewPosY = NewPosY;
+		NewPosZ += (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+		Backwards = true;
+		Driving = false;
 
-		if (acceleration < -10)
+		if (Acceleration < -1)
 		{
-			acceleration = -10;
+			Acceleration = -1;
+		}
+		if (VelocityZ < -1)
+		{
+			VelocityZ = -1;
 		}
 
 	}
-	if (backwards)
-	{
-		if (!Application::IsKeyPressed('S'))
-		{
-			//velocityZ = 0;
-			acceleration += 0.5;
-			VelocityZ += (float)(acceleration * dt);
 
-			if (acceleration > 0)
+	if (Backwards)
+	{
+		if (!Application::IsKeyPressed('K'))
+		{
+			Acceleration += 0.1;
+			VelocityZ += Acceleration * dt;
+			NewPosX += (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			NewPosY = NewPosY;
+			NewPosZ += (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+
+			if (Acceleration > 0)
 			{
-				acceleration = 0;
-				backwards = false;
+				Acceleration = 0;
+				VelocityZ += 0.05;
+			}
+			if (VelocityZ > 0)
+			{
+				VelocityZ = 0;
 			}
 		}
 	}
 
-
 }
 
-float c_FirstCar::GetVelocityZ()
+float c_FirstCar::GetPosX()
 {
-	return VelocityZ;
+	return NewPosX;
 }
+
+float c_FirstCar::GetPosY()
+{
+	return NewPosY;
+}
+
+float c_FirstCar::GetPosZ()
+{
+	return NewPosZ;
+}
+
 
