2d35214c9c1fef4a69d5933c0c697e42ae191252
Edward095 <42329335+Edward095@users.noreply.github.com>
2019-02-18 14:37:09 +0800

    Revert "Revert "Revert "Merge"""
    This reverts commit 696785d79b96cc058171a9cef1d189581217b4bc.

--- OpenGL/Source/c_FirstCar.cpp
+++ OpenGL/Source/c_FirstCar.cpp
@@ -15,11 +15,149 @@ c_FirstCar::c_FirstCar()
 	SteeringAngle = 0;
 	Duration = 0;
 
-	once = false;
 }
 c_FirstCar::~c_FirstCar()
 {
 
+}
+
+void c_FirstCar::Movement(double dt)
+{
+	
+	if (Application::IsKeyPressed('W') && Backwards == false)
+	{
+		Acceleration += 0.1;
+		VelocityZ += Acceleration * dt;
+
+		float updateX = (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+		float updateZ = (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+		if (!gotCollide(updateX, pos.y, updateZ))
+		{
+			Driving = true;
+			Backwards = false;
+			Ability(dt);
+			if (Acceleration > 1)
+				Acceleration = 1;
+			if (VelocityZ > 1 && (PressQ || Nitro))
+				VelocityZ = 1.5;
+			else if (VelocityZ > 1 && (!PressQ || !Nitro))
+				VelocityZ = 1;
+		}
+		else
+		{
+			Acceleration -= 0.1;
+			VelocityZ -= Acceleration * dt;
+		}
+		
+	}
+	if (Driving)
+	{
+		if (!Application::IsKeyPressed('W'))
+		{
+			Acceleration -= 0.1;
+			VelocityZ += Acceleration * dt;
+
+			float updateX = (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			float updateZ = (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			if (!gotCollide(updateX, pos.y, updateZ))
+			{
+				if (Acceleration < 0)
+				{
+					Acceleration = 0;
+					VelocityZ -= 0.05;
+				}
+				if (VelocityZ < 0)
+				{
+					VelocityZ = 0;
+					Driving = false;
+					Backwards = false;
+				}
+			}
+			else
+			{
+				Acceleration -= 0.1;
+				VelocityZ -= Acceleration * dt;
+			}
+		}
+	}
+
+
+	if (Application::IsKeyPressed('D'))
+	{
+		if (Driving)
+			SteeringAngle -= 3;
+		if (Backwards)
+			SteeringAngle += 3;
+		
+	}
+
+
+	if (Application::IsKeyPressed('A'))
+	{
+		if (Driving)
+			SteeringAngle += 3;
+		if (Backwards)
+			SteeringAngle -= 3;
+	}
+
+
+	if (Application::IsKeyPressed('S') && Driving == false)
+	{
+		Acceleration -= 0.1;
+		VelocityZ += Acceleration * dt;
+
+		float updateX = (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+		float updateZ = (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+
+		if (!gotCollide(updateX, pos.y, updateZ))
+		{
+			Backwards = true;
+			Driving = false;
+			Ability(dt);
+			if (Acceleration < -1)
+				Acceleration = -1;
+			if (VelocityZ < -1 && (PressQ || Nitro))
+				VelocityZ = -2;
+			else if (VelocityZ < -1 && (!PressQ || !Nitro))
+				VelocityZ = -1;
+		}
+		else
+		{
+			Acceleration += 0.1;
+			VelocityZ -= Acceleration * dt;
+		}
+	}
+
+	if (Backwards)
+	{
+		if (!Application::IsKeyPressed('S'))
+		{
+			Acceleration += 0.1;
+			VelocityZ += Acceleration * dt;
+
+			float updateX = (sin(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			float updateZ = (cos(Math::DegreeToRadian(SteeringAngle)) * VelocityZ);
+			if (!gotCollide(updateX, pos.y, updateZ))
+			{
+				if (Acceleration > 0)
+				{
+					Acceleration = 0;
+					VelocityZ += 0.05;
+				}
+				if (VelocityZ > 0)
+				{
+					VelocityZ = 0;
+					Driving = false;
+					Backwards = false;
+				}
+			}
+			else
+			{
+				Acceleration -= 0.1;
+				VelocityZ -= Acceleration * dt;
+			}
+		}
+	}
 }
 
 void c_FirstCar::Ability(double dt)
@@ -39,25 +177,6 @@ void c_FirstCar::Ability(double dt)
 			Duration = 0;
 		}
 	}
-	//if (Application::IsKeyPressed('Q') && !once)
-	//{
-	//	float updateX = (sin(Math::DegreeToRadian(SteeringAngle)) * 75);
-	//	float updateZ = (cos(Math::DegreeToRadian(SteeringAngle)) * 75);
-
-	//	updatePos(pos.x + updateX, pos.y, pos.z + updateZ);
-	//	PressQ = true;
-	//	once = true;
-	//}
-	//if (PressQ && once)
-	//{
-	//	Duration++;
-	//	if (Duration >= 150) // 3 sec/dt
-	//	{
-	//		PressQ = false;
-	//		once = false;
-	//		Duration = 0;
-	//	}
-	//}
 }
 
 void c_FirstCar::PowerUp(bool check)
